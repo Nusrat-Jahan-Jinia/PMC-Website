d7788ebd6216b3be0c18d6cdec9afdf1d84edea1
nusrat jahan <nusrat.jahan@dsinnovators.com>
2022-12-06 16:58:23 +0600

    Legend Deca font family added and custom yellow color added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}",
+  './components/**/*.{html,js}',],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pmc: {
+          100: "#AF9C3F",
+        },
+      },
+    },
+    fontFamily: {
+      headline: ['Lexend Deca', 'sans-serif'],
+    },
   },
   plugins: [],
-}
+};
