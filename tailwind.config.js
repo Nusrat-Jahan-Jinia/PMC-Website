9c7e85426435e17ebc03ad442871ece5c373db6a
nusrat jahan <nusrat.jahan@dsinnovators.com>
2022-12-12 17:31:05 +0600

    Executive page done with responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./components/**/*.{html,js}","./node_modules/flowbite/**/*.js"],
+  content: [
+    "./src/**/*.{html,js}",
+    "./components/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
     extend: {
       colors: {
@@ -10,8 +14,8 @@ module.exports = {
       },
     },
     fontFamily: {
-      myfont: ["Lexend Deca", "sans-serif"],
-      headfont: ["Playfair Display", "serif"],
+      deca: ["Lexend Deca", "sans-serif"],
+      display: ["Playfair Display", "serif"],
     },
   },
   plugins: [require("flowbite/plugin")],
